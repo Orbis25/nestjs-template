e0a0b9d66fb4a05f4f4fe3e7abe47898fb33c5ae
Orbis Alonzo Gutierrez <ogutierrez@apap.com.do>
2021-02-19 10:44:36 -0400

    swagger and validations dto

--- src/modules/users/dto/user-auth.dto.ts
+++ src/modules/users/dto/user-auth.dto.ts
@@ -1,5 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty,IsEmail } from 'class-validator';
 export class UserAuthDto {
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsNotEmpty()
   password: string;
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ required: true, uniqueItems: true })
   email: string;
 }
 
